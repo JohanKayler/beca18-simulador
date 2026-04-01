500d2d490a1a66d3ec2c411dc4c266e288d69dab
Johan <johan.moreno.cs@gmail.com>
2026-04-01 00:11:43 -0500

    Primer commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from modelo_simple import mi_orden_merito
+
+st.title("Simulador Beca 18 - Segunda Lista")
+
+st.write("Consulta tu posición estimada en la lista de no seleccionados")
+
+dni = st.text_input("Ingresa tu DNI")
+
+if st.button("Consultar"):
+    resultado = mi_orden_merito(dni)
+    
+    if resultado:
+        st.success("Resultado encontrado")
+        st.write(f"Puntaje: {resultado['puntaje']}")
+        st.write(f"Orden de mérito: {resultado['orden']} de {resultado['total']}")
+        st.write(f"Top {resultado['percent']}%")
+        st.write(resultado["mensaje"])
+    else:
+        st.error("DNI no encontrado")
+
+st.markdown("---")
+st.caption("⚠️ Esta herramienta es solo una estimación y no garantiza resultados oficiales.")
---
+++ modelo_simple.py
@@ -0,0 +1,47 @@
+import pandas as pd 
+
+df_seleccionados = pd.read_csv("data/postulantes_seleccionados.csv")
+df_no_seleccionados = pd.read_csv("data/postulantes_no_seleccionados.csv")
+
+df = pd.concat([df_seleccionados,df_no_seleccionados],  ignore_index=True)
+print(df_seleccionados)
+#Limpiamos las columnas importantes
+df_limpio = df[["MODALIDAD_BECA","DNI", "IES","CARRERA","CONCEPTO_A","CONCEPTO_B","PUNTAJE_FINAL","CONDICION"]]
+
+#Trabajaremos con solo la modalidad ordinaria
+df_limpio = df_limpio[df["MODALIDAD_BECA"]=="BECA 18 ORDINARIA"]
+
+def mi_orden_merito(dni):
+    no_sel = df_limpio[df_limpio["CONDICION"] == "NO SELECCIONADO"].copy()
+    
+    no_sel = no_sel.sort_values("PUNTAJE_FINAL", ascending=False).reset_index(drop=True)
+    no_sel["ORDEN_MERITO"] = no_sel.index + 1
+    
+    resultado = no_sel[no_sel["DNI"] == dni]
+    
+    if resultado.empty:
+        return "DNI no encontrado entre no seleccionados"
+    
+    orden = int(resultado["ORDEN_MERITO"].values[0])
+    puntaje = float(resultado["PUNTAJE_FINAL"].values[0])
+    total = len(no_sel)
+    
+    # Probabilidad aproximada
+    top_percent = round((orden / total) * 100, 2)
+    
+    # Lógica simple de estimación
+    if orden <= 2173:
+        mensaje = "🟢 Alta probabilidad de ser seleccionado en segunda lista"
+    elif orden <= 3000:
+        mensaje = "🟡 Probabilidad media (depende de reasignaciones)"
+    else:
+        mensaje = "🔴 Baja probabilidad"
+    
+    return {
+        "dni": dni,
+        "puntaje": puntaje,
+        "orden_merito": orden,
+        "total_no_seleccionados": total,
+        "top_percent": top_percent,
+        "estimacion": mensaje
+    }
